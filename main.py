128fabf87b862d27374e3793c9ab373a581f0a84
BryanAlzate007 <bryanalzate007@gmail.com>
2022-12-05 14:01:19 -0500

    archivo final

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import os
 import glob
+from zipfile import ZipFile
 
 print("descomprimir archivos con python")
-ruta = r'home/bryan-ubuntu/Documentos/usb'
+ruta = r'C:\Users\USER2021\BROWNFIELD'
 
 extencion = '*.zip'
+print(extencion)
 listaDeArchivos = glob.glob(ruta + os.sep + extencion)
 
 print(listaDeArchivos)
+
+for i in range(0,len(listaDeArchivos)):
+     with ZipFile(file = listaDeArchivos[i], mode = "r", allowZip64=True) as file:
+        listaDeArchivos[i] = file.open(name = file.namelist()[0], mode = "r")
+        print(listaDeArchivos[i].read())
+        listaDeArchivos[i].close()
+
+        navegacion = "C:\\Users\\USER2021\\BROWNFIELD"
+        print("Descomprimiendo")
+        file.extractall(path = navegacion)
+        print("archivo descomprimido")
